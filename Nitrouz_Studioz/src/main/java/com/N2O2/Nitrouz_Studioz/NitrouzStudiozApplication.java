7ba11038940628c18e00c67aacb9c7ffcf309507
kcbar <Kbarter@mail.greenriver.edu>
2019-03-25 14:31:16 -0700

    Project with gradle build!

---
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/NitrouzStudiozApplication.java
@@ -0,0 +1,13 @@
+package com.N2O2.Nitrouz_Studioz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitrouzStudiozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitrouzStudiozApplication.class, args);
+	}
+
+}
---
+++ Nitrouz_Studioz/src/test/java/com/N2O2/Nitrouz_Studioz/NitrouzStudiozApplicationTests.java
@@ -0,0 +1,16 @@
+package com.N2O2.Nitrouz_Studioz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NitrouzStudiozApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
