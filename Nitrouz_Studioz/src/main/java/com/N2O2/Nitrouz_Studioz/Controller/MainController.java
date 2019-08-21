adce065385ef39c226d4c3e0b087a6ecefdc2241
kcbar <Kbarter@mail.greenriver.edu>
2019-08-21 13:58:53 -0700

    Starting on about page and changed navbar to footer

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Controller/MainController.java
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Controller/MainController.java
@@ -1,18 +1,20 @@
 package com.N2O2.Nitrouz_Studioz.Controller;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String home_page() {
+    public String home_page(HttpSession session) {
         return "index";
     }
 
-    @RequestMapping("/about")
-    public String about_page(){
+    @GetMapping("/about")
+    public String about_page(HttpSession session){
         return "about";
     }
 }
--- Nitrouz_Studioz/src/main/resources/static/js/homepage.js
+++ Nitrouz_Studioz/src/main/resources/static/js/homepage.js
@@ -1,5 +1,7 @@
 var header = document.getElementById("nav");
 var btns = header.getElementsByClassName("navbar");
+// var activeButton = document.getElementsByClassName("home");
+// activeButton.className.replace(" home", " active");
 
 for(var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
