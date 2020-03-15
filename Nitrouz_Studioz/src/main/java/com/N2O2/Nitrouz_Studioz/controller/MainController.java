81fb827a5884a951c9cda4922d958126946f4e3e
kcbar <Kbarter@mail.greenriver.edu>
2020-03-15 16:41:18 -0700

    Finished login form and going to work on sign up form and forgot password forms next.

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/controller/MainController.java
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/controller/MainController.java
@@ -3,27 +3,62 @@ package com.N2O2.Nitrouz_Studioz.controller;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+    private boolean loggedIn = false;
+
     @GetMapping("/")
     public String home_page(HttpSession session) {
+
+        if(loggedIn){
+            String userName = "KevanBaller";
+            session.setAttribute("UserName", userName);
+            return "index";
+        }
         return "index";
     }
 
     @GetMapping("/about")
     public String about_page(HttpSession session){
+        if(loggedIn){
+            String userName = "KevanBaller";
+            session.setAttribute("UserName", userName);
+            return "about";
+        }
         return "about";
     }
 
     @GetMapping("/signup")
     public String sign_up(HttpSession session){
+        if(loggedIn){
+            String userName = "KevanBaller";
+            session.setAttribute("UserName", userName);
+            return "signup";
+        }
         return "signup";
     }
 
     @GetMapping("/login")
     public String log_in(HttpSession session){
         return "login";
+    }
+
+    @PostMapping("/Log_In")
+    public String logInAttmpt(
+        @RequestParam(name = "email") String email,
+        @RequestParam(name = "password") String password){
+
+        loggedIn = true;
+        return "redirect:/index";
+    }
+
+    @GetMapping("/logOut")
+    public String logOut(){
+        loggedIn = false;
+        return "redirect:/index";
     }
 }
