816ac2a49c42bd429cbc9983ccb11bbf3ca7b0ca
kcbar <Kbarter@mail.greenriver.edu>
2020-05-16 22:25:11 -0700

    Added confirmEmail method that sends an email to a new user along with a confirmation url

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Model/service/ProfileService.java
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Model/service/ProfileService.java
@@ -6,6 +6,8 @@ import com.N2O2.Nitrouz_Studioz.model.profile.ProfileDoa;
 import com.N2O2.Nitrouz_Studioz.model.profile.ProfileEntity;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,12 +19,15 @@ public class ProfileService {
     @Autowired
     private VerificationTokenDoa verificationTokenDoa;
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
     public void createProfile(String profileName, String email, String password, boolean join){
         profileEntity = new ProfileEntity(null, profileName, null, email, null, null, password,
             join, join, join, join, false);
         profileDoa.save(profileEntity);
 
-        confirmEmail(email, profileEntity);
+        confirmEmail(profileEntity);
     }
 
     public boolean emailExists(String email){
@@ -30,11 +35,21 @@ public class ProfileService {
     }
 
 
-    private void confirmEmail(String email, ProfileEntity profileEntity){
+    private void confirmEmail(ProfileEntity profileEntity){
         String token = UUID.randomUUID().toString();
         verificationTokenEntity = new VerificationTokenEntity(token, profileEntity);
         verificationTokenDoa.save(verificationTokenEntity);
 
-        
+        String email = profileEntity.getEmail();
+        String subject = "Please Confirm Your Email";
+        String confirmationURL = "/registrationComplete.html?token=" + token;
+        String message = "Thanks for creating and joining th community of Nitrouz Studioz."
+            + "/nThere is one last step to activate your account. Please click the link below to activate your account!";
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message + "\r\n" + "http://localhost:8080" + confirmationURL);
+        javaMailSender.send(simpleMailMessage);
     }
 }
