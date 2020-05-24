fdd44b04e64ce8e8b0beee4e77f8972a8f2d076f
kcbarter <35271923+kcbarter@users.noreply.github.com>
2020-05-23 17:29:45 -0700

    Delete VerificationTokenDoa.java

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Model/newProfileRegistration/VerificationTokenDoa.java
+++
@@ -1,9 +0,0 @@
-package com.N2O2.Nitrouz_Studioz.model.newProfileRegistration;
-
-import javax.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Transactional
-public interface VerificationTokenDoa extends JpaRepository<VerificationTokenEntity, Long> {
-
-    public VerificationTokenEntity findByToken(String token);
-}
