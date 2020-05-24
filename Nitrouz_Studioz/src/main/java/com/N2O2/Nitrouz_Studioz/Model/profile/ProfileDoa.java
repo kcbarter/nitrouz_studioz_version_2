0cf98cf91b73c14b4deaa7ff8b00eddab899d5f6
kcbarter <35271923+kcbarter@users.noreply.github.com>
2020-05-23 17:30:15 -0700

    Delete ProfileDoa.java

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/Model/profile/ProfileDoa.java
+++
@@ -1,9 +0,0 @@
-package com.N2O2.Nitrouz_Studioz.model.profile;
-
-import javax.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Transactional
-public interface ProfileDoa extends JpaRepository<ProfileEntity, Long> {
-
-    public ProfileEntity findByEmail(String email);
-}
