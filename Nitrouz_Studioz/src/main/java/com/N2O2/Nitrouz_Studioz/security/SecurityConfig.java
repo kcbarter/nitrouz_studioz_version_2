2cae9cf6b3683f2c92eace02d071cd9112f61c28
kcbar <Kbarter@mail.greenriver.edu>
2020-06-25 22:51:31 -0700

    Temporaily solved a bug and will refactor later after completing login.

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/security/SecurityConfig.java
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/security/SecurityConfig.java
@@ -8,16 +8,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
+        //todo put permitAll above athenticated
         http
             .authorizeRequests()
-                .antMatchers( "/", "/css/main.css", "/js/about.js", "/js/homepage.js",
-                    "/js/signup.js", "/images/background.jpg", "/images/N2Oz_Nitrouz_Studioz.png",
-                    "/images/Nitrouz.png", "/images/Nitrouz_Studioz.png",
-                    "/images/secondary_background.jpg", "/images/Studioz.png", "/images/twitch.png",
-                    "/images/twitter.png", "/images/Youtube.png", "/about", "/signup",
-                    "/signUpFormError", "/Log_In", "/logOut", "/ForgotPasswordPage", "/Forgot_Password",
-                    "/SignUp", "/registrationComplete").permitAll()
             .antMatchers().authenticated()
+            .antMatchers( "/", "/css/main.css", "/js/about.js", "/js/homepage.js",
+                "/js/signup.js", "/images/background.jpg", "/images/N2Oz_Nitrouz_Studioz.png",
+                "/images/Nitrouz.png", "/images/Nitrouz_Studioz.png",
+                "/images/secondary_background.jpg", "/images/Studioz.png", "/images/twitch.png",
+                "/images/twitter.png", "/images/Youtube.png", "/about", "/signup",
+                "/signUpFormError", "/Log_In", "/logOut", "/ForgotPasswordPage", "/Forgot_Password",
+                "/SignUp", "/registrationComplete").permitAll()
             .and()
             .csrf().disable();
     }
