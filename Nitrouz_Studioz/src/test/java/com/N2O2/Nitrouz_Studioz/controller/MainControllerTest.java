67fe1063715fa7594367cf9061924625b21588a4
kcbar <Kbarter@mail.greenriver.edu>
2020-07-20 19:17:46 -0700

    Added Test Security Config class in test and trying to fix bean issues I'm having in test.

--- Nitrouz_Studioz/src/test/java/com/N2O2/Nitrouz_Studioz/controller/MainControllerTest.java
+++ Nitrouz_Studioz/src/test/java/com/N2O2/Nitrouz_Studioz/controller/MainControllerTest.java
@@ -2,6 +2,7 @@ package com.N2O2.Nitrouz_Studioz.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
+import com.N2O2.Nitrouz_Studioz.NitrouzStudiozApplication;
 import com.N2O2.Nitrouz_Studioz.model.profile.ProfileDoa;
 import com.N2O2.Nitrouz_Studioz.model.profile.ProfileEntity;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,13 +23,14 @@ import org.springframework.ui.Model;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(MainController.class)
+@ContextConfiguration(classes = NitrouzStudiozApplication.class)
 public class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     ProfileEntity profileEntity;
     @Autowired
-    ProfileDoa profileDoa;
+    private ProfileDoa profileDoa;
 
     private MainController mainController;
     @Mock
