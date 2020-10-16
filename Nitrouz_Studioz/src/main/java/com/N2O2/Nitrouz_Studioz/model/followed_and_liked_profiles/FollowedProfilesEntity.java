78066692abc1e6b5515316b0c4b89686c05dc741
Kevan <kcbarter@msn.com>
2020-10-15 15:32:45 -1000

    Started on FollowedProfileEntity Class

--- Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/model/followed_and_liked_profiles/FollowedProfilesEntity.java
+++ Nitrouz_Studioz/src/main/java/com/N2O2/Nitrouz_Studioz/model/followed_and_liked_profiles/FollowedProfilesEntity.java
@@ -1,4 +1,14 @@
 package com.N2O2.Nitrouz_Studioz.model.followed_and_liked_profiles;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "followed_profiles")
 public class FollowedProfilesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "followed _profile_id")
+    private long id;
+
 }
